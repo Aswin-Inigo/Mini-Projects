2de4071115adec14b20f45004eca6dfda59abbe1
Aswin Inigo <aswininigo@virtualmaze.co.in>
2025-07-22 13:11:37 +0530

    added digital

--- Clock/index.js
+++ Clock/index.js
@@ -10,4 +10,7 @@ setInterval(() => {
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
+    dighour.innerText= `${htime < 10 ? `0${htime}` : htime}${" "}:`;
+    digmin.innerText= `${mtime < 10 ? `0${mtime}`: mtime} ${" "}:`;
+    digsec.innerText= `${stime < 10 ? `0${stime}` : stime} ${" "}`;
 }, 1000);
